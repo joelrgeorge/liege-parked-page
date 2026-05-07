42fd893c783cc97d14a6a7112d42a013a97fec86
Joel R. George <georgejoel955@gmail.com>
2026-05-07 22:10:07 +0530

    renamed App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 // ================================
-// FILE: src/main.jsx
+// FILE:  src/main.jsx
 // ================================
 import React from 'react'
 import ReactDOM from 'react-dom/client'
